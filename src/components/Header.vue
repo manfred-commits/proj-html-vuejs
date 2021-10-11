b0ff26792afff0eaf5afe92e12311caeaf7c172e
manfred-commits <manfredipiraino@gmail.com>
2021-10-11 11:51:52 +0200

    Project Start

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <div class="left-col">
+        <img src="" alt="">
+    </div>  
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
