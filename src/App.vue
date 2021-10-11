38bbd3f102fa32244b087f3cf674eb3968cbdf59
manfred-commits <manfredipiraino@gmail.com>
2021-10-11 15:39:03 +0200

    Added Header component dynamic/scss

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Header/>
+    <Header :headerNavigationList="navListHeader"/>
     <Main/>
     <Footer/>
   </div>
@@ -10,7 +10,7 @@
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import '@fortawesome/fontawesome-free';
+import '@fortawesome/fontawesome-free/js/all.js';
 
 
 export default {
@@ -19,6 +19,42 @@ export default {
     Header,
     Main,
     Footer
+  },
+  data(){
+    return{
+      navListHeader:[
+        {
+          name:'Home',
+          link:'#',
+          state:false,
+        },
+        {
+          name:'Services',
+          link:'#',
+          state:false,
+        },
+        {
+          name:'About',
+          link:'#',
+          state:false,
+        },
+        {
+          name:'Videos',
+          link:'#',
+          state:false,
+        },
+        {
+          name:'Blog',
+          link:'#',
+          state:false,
+        },
+        {
+          name:'Store',
+          link:'#',
+          state:true,
+        },
+      ]
+    }
   }
 }
 </script>
@@ -26,8 +62,27 @@ export default {
 <style lang="scss">
 
 @import './assets/style/general';
+@import '~@fortawesome/fontawesome-free/scss/fontawesome';
+
+button{
+  min-width: 270px;
+  min-height: 60px;
+  border-radius: 2.8125rem;
+  color: $primary_btn_color;
+  background-color: $primary_btn_background_color;
+  border: 0;
+  margin: 0 15px;
+}
+
 
 #app {
   
+  .page-margins{
+    width: 70%;
+    margin: auto;
+
+
+  }
+
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,128 @@
 <template>
   <header>
-    <div class="left-col">
-        <img src="" alt="">
-    </div>  
+    <div class="page-margins">
+
+        <div class="left-col">
+            <img src="../assets/images/logo.png" alt="Brand logo header">
+        </div> 
+
+        <div class="right-col">
+
+            <nav>
+                <section id="page-navigation">
+                    <ul>
+                        <li v-for="(headerElement,index) in headerNavigationList" :key="headerElement.name+index">
+                            <a :href="headerElement.link">{{headerElement.name}} <span v-if="headerElement.state==true" class="yellow-popup">new</span></a>
+                        </li>
+                    </ul>
+                    <button>Schedule a workout</button>
+                    <i class="fas fa-shopping-cart icon-formatting"></i>
+                    <i class="fas fa-search icon-formatting"></i>
+                </section>
+
+
+            </nav>
+
+        </div>   
+
+
+    </div>
   </header>
 </template>
 
 <script>
 export default {
     name: 'Header',
+    props:['headerNavigationList'],
+    
 }
 </script>
 
 <style lang="scss" scoped>
 
+@import '../assets/style/variables';
+
+
+header{
+    background-color: $header_background_color;
+    height: 15vh;
+    color: $header_primary_text_color;
+    font-size: 20px;
+
+
+    .page-margins{
+
+        @include header_components_verticalization;
+        justify-content: space-between;
+    
+        .left-col{
+    
+        }
+
+        .right-col{
+            height: 100%;
+            display: flex;
+            align-items: center;
+            flex-grow: 1;
+            
+
+            nav{
+
+                @include header_components_verticalization;
+
+                align-items: center;
+
+                #page-navigation{
+
+                    @include header_components_verticalization;
+                    justify-content: flex-end;
+
+                    ul{
+                        list-style: none;
+                        display: flex;
+                        align-items: center;
+                        margin: 0;
+                        
+    
+                        li{
+                            margin: 0 20px;
+                            
+                            a{
+                                text-decoration: none;
+                                line-height: 16px;
+                                color: $header_primary_text_color;
+                            }
+                            .yellow-popup{
+                                display: inline-flex;
+                                color: $primary_background_popup_color_text;
+                                justify-content: center;
+                                align-items: center;
+                                background-color: $primary_background_popup_color;
+                                width: 55px;
+                                height: 30px;
+                                border-radius: 5px;
+                                vertical-align: bottom;
+                            }
+                        }
+                        :hover a {
+                            color: $header_hover_primary_text_color;
+;
+                        }
+                    }
+    
+                    
+                    .icon-formatting{
+                    
+                        margin: 5px 20px;
+                    }
+                    .icon-formatting:last-child{
+                        margin-right: 0;
+                    }
+                }
+            }
+            
+        }
+
+    }
+}
 </style>
