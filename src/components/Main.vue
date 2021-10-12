71371c1d337ad4148478fe5bbc0cb1012ebb7680
manfred-commits <manfredipiraino@gmail.com>
2021-10-12 11:23:53 +0200

    added section fitness & mailing

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,39 @@
 <template>
   <main>
 
-    <!-- fitness professional section -->
+    <!-- fitness list section -->
     <section class="fitnes-professional">
+        <div class="page-margins section-formatting">
+            <h4>Certified fitness professional</h4>
+            <h2>Take control of your health</h2>
+
+            <div class="fitness-hr">
+                <i class="icon-formatting-left fas fa-chevron-up"></i>
+                <i class="icon-formatting-center fas fa-chevron-up"></i>
+                <i class="icon-formatting-right fas fa-chevron-up"></i>
+            </div>
+
+            <div class="description-text">
+                Curabitur non nulla sit amet nisl tempus convallis quis ac lectus dolor sit amet.
+            </div>
+
+            <button class="button-page">
+                <i class="fab fa-youtube"></i>
+                Visit my YouTube channel
+            </button>
+
+            <button class="button-page">
+                Buy Avada today
+                <i class="fas fa-arrow-right"></i>
+            </button>
+        </div>
+
+    </section>
+    <!-- /fitness list section -->
+
+
+    <!-- mailing list section -->
+    <section class="mailing-list">
         <div class="page-margins section-formatting">
             <h4>Join our mailing list today</h4>
             <h2>Insider offers & flash sales in your inbox every week.</h2>
@@ -14,12 +45,18 @@
             </div>
 
             <input type="text" placeholder="Insert your email...*">
-            <button>Subscribe</button>
+            <button class="button-page">Subscribe</button>
 
-            <p>Curabitur non nulla sit amet nisl tempus convallis quis ac lectus dolor sit amet, consectetur adipiscing elit sed porttitor lectus.</p>
+            <div class="description-text">
+                Curabitur non nulla sit amet nisl tempus convallis quis ac lectus dolor sit amet, consectetur adipiscing elit sed porttitor lectus.
+            </div>
+
         </div>
 
     </section>
+    <!-- mailing list section -->
+
+
 
 
   </main>
@@ -33,39 +70,51 @@ export default {
 
 <style lang="scss" scoped>
 
+@import '../assets/style/variables';
+
 
 .fitnes-professional{
-    background-image:linear-gradient(90deg, rgba(0, 0, 1, 1)40%, rgba(0, 0, 1, 0.1) 60%),url('../assets/images/banner1-2x.jpg');    
-    background-position: right;
+    background-image:linear-gradient(90deg, $header_background_color 47%, rgba(0, 0, 1, 0.1) 60%),url('../assets/images/slider82x-scaled.jpg');    
+    background-position: top;
     background-size: cover;
     background-repeat: no-repeat;
 
-    
-    .section-formatting{
-
-        padding: 140px 0 350px;
-        h4,h2,p{
-
-            width: 50%;
-        }
-
-        h4{
-            font-size: 30px;
-            color: red;
-        }
-        h2{
-            font-size: 60px;
-            color: white;
-        }
-        p{
-            color: grey;
-            font-size: 28px;
-        }
-        
-
+    button{
+        margin-left: 0;
     }
+    
 
 }
+.mailing-list{
+    background-image:linear-gradient(90deg, $header_background_color 47%, rgba(0, 0, 1, 0.1) 60%),url('../assets/images/banner1-2x.jpg');    
+    background-position: right;
+    background-size: cover;
+    background-repeat: no-repeat;
+    
+}
 
+.section-formatting{
+
+    padding: 140px 0 280px;
+    h4,h2,.description-text{
+        width: 60%;
+    }
+
+    h4{
+        font-size: 30px;
+        color: $mailing_secondary_text_color;
+    }
+    h2{
+        font-size: 80px;
+        color: $mailing_primary_text_color;
+    }
+    .description-text{
+        color: $mailing_description_text_color;
+        font-size: 28px;
+        margin-bottom: 50px;
+    }
+    
+
+}
 
 </style>
