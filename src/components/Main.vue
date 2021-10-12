95133dea16070c5a34c27b7e0d19f9e4a02f2dc4
manfred-commits <manfredipiraino@gmail.com>
2021-10-12 10:11:56 +0200

    added custom hr and scss styling

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -7,11 +7,16 @@
             <h4>Join our mailing list today</h4>
             <h2>Insider offers & flash sales in your inbox every week.</h2>
 
-            <hr>
+            <div class="fitness-hr">
+                <i class="icon-formatting-left fas fa-chevron-up"></i>
+                <i class="icon-formatting-center fas fa-chevron-up"></i>
+                <i class="icon-formatting-right fas fa-chevron-up"></i>
+            </div>
+
+            <input type="text" placeholder="Insert your email...*">
+            <button>Subscribe</button>
 
             <p>Curabitur non nulla sit amet nisl tempus convallis quis ac lectus dolor sit amet, consectetur adipiscing elit sed porttitor lectus.</p>
-            <button>Visit my YouTube channel</button>
-            <button>But Avada today</button>
         </div>
 
     </section>
@@ -30,24 +35,33 @@ export default {
 
 
 .fitnes-professional{
-    background-image:linear-gradient(to left, rgba(0, 0, 0,0), rgba(0, 0, 0,1) 62%),url('../assets/images/banner1-2x.jpg');
-    background-position: center;
+    background-image:linear-gradient(90deg, rgba(0, 0, 1, 1)40%, rgba(0, 0, 1, 0.1) 60%),url('../assets/images/banner1-2x.jpg');    
+    background-position: right;
     background-size: cover;
     background-repeat: no-repeat;
 
     
     .section-formatting{
 
-        padding: 140px 0 230px;
+        padding: 140px 0 350px;
+        h4,h2,p{
+
+            width: 50%;
+        }
+
         h4{
             font-size: 30px;
             color: red;
         }
         h2{
             font-size: 60px;
-            width: 80%;
             color: white;
         }
+        p{
+            color: grey;
+            font-size: 28px;
+        }
+        
 
     }
 
