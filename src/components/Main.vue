a1ebcda9c3ec81c70bd3c4878a8a081d6a6433c2
manfred-commits <manfredipiraino@gmail.com>
2021-10-13 10:04:49 +0200

    added basic endurance list section in main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -37,6 +37,91 @@
     <!-- /fitness list section -->
 
 
+    <!-- endurance list section -->
+    <section class="endurance-list">
+        <div class="page-margins section-formatting">
+
+            <div class="card-container">
+                <!-- crossfit card -->
+                <div class="basic-card">
+                    <img src="../assets/images/service6-2x.jpg" alt="girl drinking">
+
+                    <div class="hovering_box">
+
+                        <div class="fitness-hr">
+                            <i class="icon-formatting-left fas fa-chevron-up"></i>
+                            <i class="icon-formatting-center fas fa-chevron-up"></i>
+                            <i class="icon-formatting-right fas fa-chevron-up"></i>
+                        </div>
+
+                        <h3>Crossfit workout</h3>
+                        <h5>Push your limits</h5>
+
+                    </div>
+                </div>
+                <!-- /crossfit card -->
+
+                <!-- crossfit card -->
+                <div class="basic-card">
+                    <img src="../assets/images/box1-2x.jpg" alt="girl drinking">
+
+                    <div class="hovering_box">
+
+                        <div class="fitness-hr">
+                            <i class="icon-formatting-left fas fa-chevron-up"></i>
+                            <i class="icon-formatting-center fas fa-chevron-up"></i>
+                            <i class="icon-formatting-right fas fa-chevron-up"></i>
+                        </div>
+
+                        <h3>New gym apparel</h3>
+                        <h5>Look good, feel good</h5>
+
+                    </div>
+                </div>
+                <!-- /crossfit card -->
+
+                <!-- crossfit card -->
+                <div class="basic-card">
+                    <img src="../assets/images/box3-2x.jpg" alt="girl drinking">
+
+                    <div class="hovering_box">
+
+                        <div class="fitness-hr">
+                            <i class="icon-formatting-left fas fa-chevron-up"></i>
+                            <i class="icon-formatting-center fas fa-chevron-up"></i>
+                            <i class="icon-formatting-right fas fa-chevron-up"></i>
+                        </div>
+
+                        <h3>Team training</h3>
+                        <h5>Find a partner</h5>
+
+                    </div>
+                </div>
+                <!-- /crossfit card -->
+
+            </div>
+            
+            <h2>"How you respond to the challenge in the second half will determine what you become after the game, whether you are a winner or a loser".</h2>
+
+            <div class="fitness-hr">
+                <i class="icon-formatting-left fas fa-chevron-up"></i>
+                <i class="icon-formatting-center fas fa-chevron-up"></i>
+                <i class="icon-formatting-right fas fa-chevron-up"></i>
+            </div>
+
+            <div class="testimanial-review">
+                <img src="../assets/images/avatar-1-2x.jpg" alt="brad johnson photo">
+                <h6>Brad Johnson</h6>
+                <h6 class="location">Las Vegas</h6>
+            </div>
+            
+            
+
+        </div>
+
+    </section>
+    <!-- endurance list section -->   
+
     <!-- mailing list section -->
     <section class="mailing-list">
         <div class="page-margins section-formatting">
@@ -102,23 +187,23 @@ export default {
 @import '../assets/style/variables';
 
 .section-formatting{
-
-    padding: 140px 0 280px;
+    
+    padding: 280px 0 280px;
     h4,h2,.description-text{
         width: 60%;
     }
 
     h4{
-        font-size: 30px;
+        font-size: 50px;
         color: $mailing_secondary_text_color;
     }
     h2{
-        font-size: 80px;
+        font-size: 140px;
         color: $mailing_primary_text_color;
     }
     .description-text{
         color: $mailing_description_text_color;
-        font-size: 28px;
+        font-size: 45px;
         margin-bottom: 50px;
     }
     
@@ -129,13 +214,166 @@ export default {
     background-position: top;
     background-size: cover;
     background-repeat: no-repeat;
+    position: relative;
 
     button{
         margin-left: 0;
     }
-    
+}
+
+.play-button{
+    position: absolute;
+    transform: translate(-50%,-50%);
+    top: 50%;
+    left: 80%;
+    display: flex;
+    color: black;
+    width: 150px;
+    height: 150px;
+    background-color: white;
+    border-radius: 50%;
+    a{
+        font-size: 60px;
+        vertical-align: middle;
+        margin: auto;
+        text-decoration: none;
+    }
+}
+
+
+.endurance-list{
+    background-image: linear-gradient(to bottom right, blue , red);
+    .section-formatting{
+        text-align: center;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+
+        .card-container{
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            position:relative;
+            top: -400px;
+
+            .basic-card{
+                display: flex;
+                box-shadow: 1px 2px 15px black;
+
+                position: relative;
+                width: calc(100% /3 - 80px);
+                border-radius: 5px 5px 60px 0;
+                overflow: hidden;
+
+                img{
+                    object-fit: cover;
+                    width: 100%;
+                }
+                .hovering_box{
+                   
+                    position:absolute;
+                    top: 0;
+                    left: 0;
+                    right: 0;
+                    bottom: 0;
+                    display: flex;
+                    flex-wrap: wrap;
+                    align-content: flex-end;
+                    background-image:linear-gradient(360deg, $header_background_color -10%, rgba(0, 0, 1, 0.1) 60%);  
+                    // background-color: rgba(0, 0, 1, 0.3);
+
+                    h3,h5{
+                        text-align: left;
+                        margin-left: 10%;
+                    }
+
+                    h3{
+                        color: white;
+                        width: 100%;
+                        font-size: 60px;
+                    }
+                    h5{
+                        color: yellow;
+                        width: 100%;
+                        font-size: 40px;
+
+                        margin-bottom: 15%;
+
+                    }
+
+                    .fitness-hr{
+                        height: 40px;
+                        .icon-formatting-left,
+                        .icon-formatting-center,
+                        .icon-formatting-right{
+                            font-size: 35px;
+                            color: white;
+                        }
+                        .icon-formatting-left{
+                            left:100px;
+                        }
+                        .icon-formatting-center{
+                            left: calc(100px + 23px);
+                        }
+                        .icon-formatting-right{
+                            left: calc(200px - 53px);
+                            
+                        }
+                    }
+                }
+
+            }
+        }
+
+        h2{
+            width: 80%;
+            font-size: 100px;
+        }
+        .fitness-hr{
+            width: 100%;
+            position: relative;
+            .icon-formatting-left,
+            .icon-formatting-center,
+            .icon-formatting-right{
+                position: absolute;
+                color: $yellor_page_color;
+                font-size: 60px;
+                top: 50%;
+                
+            }
+            .icon-formatting-left{
+                left:calc(50% + -43px);
+            }
+            .icon-formatting-center{
+                left: 50%;
+            }
+            .icon-formatting-right{
+                left: calc(50% + 43px);
+                
+            }
+        }
+        .testimanial-review{
+            display: flex;
+            align-items: center;
+            img{
+                border-radius: 50%;
+            }
+            h6{
+                font-size: 38px;
+                padding: 0 30px;
+                color: white;
+            }
+            .location{
+                font-weight: 400;
+                color: lightgrey;
+                padding-left: 0;
+            }
+        }
+    }
 
 }
+
+
 .mailing-list{
     background-image:linear-gradient(90deg, $header_background_color 47%, rgba(0, 0, 1, 0.1) 60%),url('../assets/images/banner1-2x.jpg');    
     background-position: right;
@@ -159,13 +397,14 @@ export default {
         padding: 240px 0 100px;
 
         h4,h2,.description-text{
-        width: 80%;
+        width: 70%;
         margin: auto;
         }    
 
         // custom hr for section
         .inspirational-hr{
             padding: 50px 0;
+            margin-bottom: 50px;
             width: 100%;
             position: relative;
 
@@ -174,21 +413,22 @@ export default {
             .icon-formatting-right{
                 position: absolute;
                 color: $yellor_page_color;
-                font-size: 40px;
-                transform: translate(-50%,-50%);
-                top: 50%;
-                left: 50%;
+                font-size: 60px;
+                top: 50%;                
+            }
+            .icon-formatting-left{
+                left:calc(50% + -43px);
             }
             .icon-formatting-center{
-                left: calc(50% + 27px);
+                left: 50%;
             }
             .icon-formatting-right{
-                left: calc(50% + 54px);
-                
+                left: calc(50% + 43px);                
             }
         }    
         .description-text{            
             color: $mailing_primary_text_color;
+            font-size: 45px;
         }
         .button-page{
             margin: 50px 150px;
@@ -196,24 +436,6 @@ export default {
     }
 }
 
-.play-button{
-    position: absolute;
-    transform: translate(-50%,-50%);
 
-    top: 50%;
-    left: 80%;
-    display: flex;
-    color: black;
-    width: 150px;
-    height: 150px;
-    background-color: white;
-    border-radius: 50%;
-    a{
-        font-size: 60px;
-        vertical-align: middle;
-        margin: auto;
-        text-decoration: none;
-    }
-}
 
 </style>
